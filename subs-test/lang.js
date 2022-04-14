7a481c9dd09bbb8a099678a68ccae1f49dc38224
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2022-04-14 17:17:39 +0300

    Create lang.js

---
+++ subs-test/lang.js
@@ -0,0 +1,12 @@
+const langArr = {
+    "subsib" : {
+        "ru": "Подписчики",
+        "en": "Subscribers",
+        "ua": "Підписники",
+    },
+    "names" : {
+        "ru": "Название канала",
+        "en": "Channel name",
+        "ua": "Назва каналу",
+    },
+}
