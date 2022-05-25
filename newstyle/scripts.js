10ccb1903716e9f78cf4e209160a254279945894
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2022-05-25 11:58:31 +0300

    Update scripts.js

--- newstyle/scripts.js
+++ newstyle/scripts.js
@@ -2,9 +2,9 @@ function locate_assign() {
 document.location.assign("https://superplaycounts.github.io/");
 }
 let header__burger = document.querySelector('.header__burger');
-let menu = document.querySelector('.menu');
+let header__menu = document.querySelector('.header__menu');
 
 header__burger.addEventListener('click', function(){
 	header__burger.classList.toggle('active');
-	menu.classList.toggle('active');
+	header__menu.classList.toggle('active');
 })
