a9bbb7f3da178b959b522cb9923ea63f3b56a3c7
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2022-10-18 10:50:47 +0300

    Update ytodo.js

--- yt-odometer/ytodo.js
+++ yt-odometer/ytodo.js
@@ -2,3 +2,5 @@ var cmm = '12345678';
 var raw = 0;
 var m = '100m';
 var id = window.location.href.split('?')[1] || 'UCq-Fj5jknLsUf-MWSy4_brA'
+var url = 'https://mixerno.space/api/youtube-channel-counter/user/'
+var path = 'data.counts[0].count'
