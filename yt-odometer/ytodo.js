ee0440edcdfcf470af454d4f70c4bd6bd2f03833
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2022-11-05 12:21:26 +0200

    Update ytodo.js

--- yt-odometer/ytodo.js
+++ yt-odometer/ytodo.js
@@ -2,7 +2,7 @@ var cmm = '12345678';
 var raw = 0;
 var m = '100m';
 var id = window.location.href.split('?')[1] || 'UCq-Fj5jknLsUf-MWSy4_brA'
-var url = 'https://mixerno.space/api/youtube-channel-counter/user/'
+var url = 'https://api-v2.nextcounts.com/api/youtube/channel/'
 var path = 'data.counts[0].count'
 function spl(n) {
     n = ''+n+''   
